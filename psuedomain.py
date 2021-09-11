d77ada7e47dfe3ad74929ceea4a8ca388a1529f1
Imran Iftikar <imran.aidan.ai@gmail.com>
2021-09-10 20:59:48 -0400

    Update psuedomain.py

--- psuedomain.py
+++ psuedomain.py
@@ -1,5 +1,7 @@
 
-####### Data ############################################################################
+# pseudo main with ugly code. will fix when added to "main.py"
+
+###### Data #############################################################################
 
 regularMin = [3.5, 3.5, 0.007]
 regularMax = [4.25, 6, 0.016]
@@ -18,30 +20,35 @@ fullMax = [regularMax, largeMax, envoMax, lenvoMax]
 
 typeList = ['regular', 'large', 'envelope', 'large envelope']
 
-parcel = [5, 11, 0.017]
+
+zipzones = {1:[1, 6999],
+            2:[7000, 19999],
+            3:[20000, 35999],
+            4:[36000, 62999],
+            5:[63000, 84999], 
+            6:[85000, 99999]}
 
 #########################################################################################
 
 class Letter:
     
-    def __init__(self, measurements, zipcode):
+    def __init__(self, measurements, zipcode, lettertype, zipsthrough):
         self.dim_list = measurements
         self.zip_list = zipcode
-        pass
+        self.type = lettertype
+        self.zones = zipsthrough
 
-    def itemtype(self, type):
-        self.type = type
 
 def grabdata():
     flag = True
-    letter_data = [4,5,0.01,"0","0"]
+    letter_data = []
     while flag:
 
         for i in range (0,3):
-            # letter_data.append(float(input("Measurement: ")))
+            letter_data.append(float(input("Measurement: ")))
             pass
         for i in range (0,2):
-            # letter_data.append(str(input("Zipcode: ")))
+            letter_data.append(str(input("Zipcode: ")))
             pass
 
         print(f"Length: {str(letter_data[0])} units, Height: {str(letter_data[1])} units, Thickness: {str(letter_data[2])} Inches, From: {str(letter_data[3])}, To: {str(letter_data[4])}")
@@ -61,17 +68,31 @@ def findSize(listmin, listmax, item, parType):
       #print(parType[j])
       return parType[j]
 
+def zonesthrough(testdict, testcodes):
+    zones = [0,0] # the two zones we pass through. idc about scalability/adaptivity atm, as the problem has no stipulations
+    for j in range (2): # two zipcodes to run through
+        for i in range(0,6): # 6 potenial regions to run through
+            i += 1 # by def of range func, i is one less than the dict key. "+=" is the quick fix
+            if testdict[i][0] <= int(testcodes[j]) <= testdict[i][1]: #simple comparing thezipcode to values for each zone
+                zones[j] = i # if zipcode matches with a zone, add zone number to list
+
+    passingthrough = (max(zones) - min(zones)) # finding number of zones passed through. zone numbers themselves irrelevant x-ept for debugging
+    return passingthrough # returns number of zones passed to the function
 
 
 
 
-letter = Letter(None, None)
+letter = Letter(None, None, None, None)
 datagrabbed = grabdata()
+
 letter.dim_list = datagrabbed[0:3]
 print(letter.dim_list)
+
 letter.zip_list = datagrabbed[3:5]
 print(letter.zip_list)
 
-letter.itemtype(str(findSize(fullMin, fullMax, parcel, typeList)))
+letter.type = str(findSize(fullMin, fullMax, letter.dim_list, typeList))
 print(letter.type)
 
+letter.zones = zonesthrough(zipzones, letter.zip_list)
+print(letter.zones)
